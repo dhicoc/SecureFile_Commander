c5ceb1e39a10c98dcff03166f9bf4f5c546e8990
yhc <1964366186@qq.com>
2025-11-06 16:06:18 +0800

    这个数据库脚本

--- data.sql
+++ data.sql
@@ -2,3 +2,4 @@ CREATE DATABASE securefile_db CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 CREATE USER 'your_username'@'localhost' IDENTIFIED BY 'your_password';
 GRANT ALL PRIVILEGES ON securefile_db.* TO 'your_username'@'localhost';
 FLUSH PRIVILEGES;
+# Run this command to create the database and user
