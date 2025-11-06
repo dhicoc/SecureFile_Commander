19ae1fc5336b11dd2502ab9acf05568ba15863b6
yhc <1964366186@qq.com>
2025-11-06 16:05:47 +0800

    这个数据库脚本

--- data.sql
+++ data.sql
@@ -1,1 +1,4 @@
-create database test;
+CREATE DATABASE securefile_db CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE USER 'your_username'@'localhost' IDENTIFIED BY 'your_password';
+GRANT ALL PRIVILEGES ON securefile_db.* TO 'your_username'@'localhost';
+FLUSH PRIVILEGES;
